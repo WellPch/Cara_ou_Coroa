61fc4f1ae3c97b5692069225ee404052e000a7e6
WELLINGTON PACHECO <wellington17-sal@hotmail.com>
2021-12-22 21:15:32 -0300

    Cara_ou_coroa

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:caraoucoroa/Jogar.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Jogar(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class CaraOuCoroa extends StatefulWidget {
+  const CaraOuCoroa({Key? key}) : super(key: key);
+
+  @override
+  _CaraOuCoroaState createState() => _CaraOuCoroaState();
+}
+
+class _CaraOuCoroaState extends State<CaraOuCoroa> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
